157071a378c04c438423b78120254f7695ab9c31
Dzakwan Irfan <lexdani368@gmail.com>
2024-01-26 20:32:24 +0700

    navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import Hero1 from "@/public/img/hero1.jpg";
 
 export default function Home() {
   return (
-    <p>hello world</p>
+    <div id="hero" className="">
+      <div className="heroContent"></div>
+      <div className="heroShadow w-full h-dvh"></div>
+      <Image 
+        src={Hero1}
+        alt="Traditional Restaurant"
+        objectFit="cover"
+      />
+    </div>
   );
 }
---
+++ app/ui/navbar.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <header
+            className="bg-neutral-50 rounded-b-3xl justify-between px-36 py-4 flex items-center fixed right-0 top-0 w-full z-50"
+        >
+            <Link
+                href='/'
+            >
+                <Image 
+                    src='/logo/text-logo.png'
+                    alt="Wareg Logo"
+                    width={201}
+                    height={60}
+                    className="w-36 hover:opacity-90"
+                />
+            </Link>
+
+            <Link
+                href='/'
+                className="absolute right-1/2 top-12"
+            >
+                <Image 
+                    src='/logo/icon-logo.png'
+                    alt="Waren Logo"
+                    width={50}
+                    height={50}
+                    className="transition-all hover:scale-150 hover:rotate-180"
+                />
+            </Link>
+
+            <Link
+                href='/'
+            >
+                <div id="sign-in" className="text-neutral-50 bg-orange-500 px-4 py-1 rounded-full hover:transition-all hover:outline hover:outline-orange-500 hover:bg-neutral-50 hover:text-neutral-800 font-bold">
+                    Sign in
+                </div>
+            </Link>
+        </header>
+    );
+}
