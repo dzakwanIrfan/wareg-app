6865a32e57dbc277ba54c4709481c3c3a88dac02
Dzakwan Irfan <lexdani368@gmail.com>
2024-01-28 21:49:27 +0700

    simple modulation

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
-import Image from "next/image";
-import Hero1 from "@/public/img/hero1.jpg";
-import { Galindo } from "next/font/google";
-import SearchBar from "./ui/home/search-bar";
-
-
-const galindo = Galindo({ 
-  weight: ['400'],
-  style: ['normal'],
-  subsets: ['latin']
- });
+import Hero from "./ui/home/hero";
 
 export default function Home() {
   return (
-    <div id="hero" className="relative">
-      <div className={`heroContent absolute z-10 top-56 left-36 text-neutral-50 text-8xl text-shadow ${galindo.className}`}>
-        <div className="line1">Kuliner Lokal,</div>
-        <div className="line2">Rating Global!</div>
-      </div>
-      <SearchBar />
-      <Image 
-        src={Hero1}
-        alt="Traditional Restaurant"
-        objectFit="cover"
-        className="contrast-75 -z-10"
-      />
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
